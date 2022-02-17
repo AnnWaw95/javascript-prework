80360b617c6abdd5e648517fcfa14fc5b19c8bbf
Ania Wawerek <wawerekanna@gmail.com>
2022-02-17 14:58:07 +0100

    Add functions.js

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var ComputerMove, item, PlayerMove;
+ComputerMove = 'kamień';
+printMessage('Zagrałem ' + ComputerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+PlayerMove = 'papier';
+printMessage('Zagrałem' + PlayerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
